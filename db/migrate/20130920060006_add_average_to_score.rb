c71f2e51f7c0a5f113f3c60c537fbcd296bb9949
eeshankulkarni <eeshankulkarni@gmail.com>
2013-10-26 12:30:56 +0800

    remove more

--- db/migrate/20130920060006_add_average_to_score.rb
+++
@@ -1,5 +0,0 @@
-class AddAverageToScore < ActiveRecord::Migration
-  def change
-    add_column :scores, :average, :float
-  end
-end
--- db/migrate/20130920060606_remove_average_from_score.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAverageFromScore < ActiveRecord::Migration
-  def change
-    remove_column :scores, :average, :float
-  end
-end
